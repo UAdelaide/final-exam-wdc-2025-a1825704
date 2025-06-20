1bec2eb0a81579a92ce1a0bdcc03df3d4cef7301
Niko <a1825704@student.adelaide.edu.au>
2025-06-20 03:35:05 +0000

    [automatic commit] 2025-06-20 03:35:05 UTC 101.174.25.68

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -12,7 +12,7 @@ try {
             size: results[0].size,
             owner_username: results[0].owner_id
         };
-        res.
+        res.json(dog);
     });
 } catch(err) {
     res.status(500).send('Error retrieving data: ' + err);

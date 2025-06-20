09fecf523c38c22f7d25e501b7e04f164852a799
Niko <a1825704@student.adelaide.edu.au>
2025-06-20 04:15:19 +0000

    [automatic commit] 2025-06-20 04:15:19 UTC 101.174.25.68

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -5,7 +5,7 @@ router.get('/api/dogs', function(req, res, next) {
 const pool = req.pool;
 
 try {
-    pool.query('SELECT Dogs.name AS dog_name, Dogs.size, Users.username FROM Dogs INNER JOIN Users ON Users.user_id = Dogs.owner_id', (err, results) => {
+    pool.query('SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS  FROM Dogs INNER JOIN Users ON Users.user_id = Dogs.owner_id', (err, results) => {
         res.send(results);
     });
 } catch(err) {
@@ -17,7 +17,7 @@ router.get('/api/walkrequests/open', function(req, res, next) {
 const pool = req.pool;
 
 try {
-    pool.query(`SELECT WalkRequests.request_id, Dogs.name AS dog_name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username AS owner_name FROM WalkRequests INNER JOIN Dogs ON Dogs.dog_id = WalkRequests.dog_id INNER JOIN Users ON Dogs.owner_id = Users.user_id WHERE status='open'`,
+    pool.query(`SELECT WalkRequests.request_id, Dogs.name AS dog_name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username AS owner_username FROM WalkRequests INNER JOIN Dogs ON Dogs.dog_id = WalkRequests.dog_id INNER JOIN Users ON Dogs.owner_id = Users.user_id WHERE status='open'`,
         (err, results) => {
             res.send(results);
         });

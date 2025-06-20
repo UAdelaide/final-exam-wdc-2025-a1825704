3dad3b91d2669ca2e268f5fd69faebe003588ce2
Niko <a1825704@student.adelaide.edu.au>
2025-06-20 04:21:47 +0000

    [automatic commit] 2025-06-20 04:21:47 UTC 101.174.25.68

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -6,6 +6,10 @@ const pool = req.pool;
 
 try {
     pool.query('SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username FROM Dogs INNER JOIN Users ON Users.user_id = Dogs.owner_id', (err, results) => {
+        if (err) {
+                console.log('Error Open Walkers:', err);
+                return res.status(500).send('Could not load walkers');
+            }
         res.send(results);
     });
 } catch(err) {
@@ -20,8 +24,8 @@ try {
     pool.query(`SELECT WalkRequests.request_id, Dogs.name AS dog_name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username AS owner_username FROM WalkRequests INNER JOIN Dogs ON Dogs.dog_id = WalkRequests.dog_id INNER JOIN Users ON Dogs.owner_id = Users.user_id WHERE status='open'`,
         (err, results) => {
             if (err) {
-                console.log('Error Open Walker:', err);
-                return res.status(500).send('Could not load users');
+                console.log('Error Open Walkers:', err);
+                return res.status(500).send('Could not load walkers');
             }
             res.send(results);
         }

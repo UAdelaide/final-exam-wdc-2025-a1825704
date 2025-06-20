caa6cd73eec57c5eceb5df154509cb4b69291196
Niko <a1825704@student.adelaide.edu.au>
2025-06-20 03:48:43 +0000

    [automatic commit] 2025-06-20 03:48:43 UTC 101.174.25.68

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -5,7 +5,7 @@ router.get('/api/dogs', function(req, res, next) {
 const pool = req.pool;
 
 try {
-    pool.query('SELECT Dogs.name, Dogs.zise', (err, results) => {
+    pool.query('SELECT Dogs.name, Dogs.size, Users.username FROM Dogs INNER JOIN ON Users', (err, results) => {
     });
 } catch(err) {
     res.status(500).send('Error retrieving data: ' + err);

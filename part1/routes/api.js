3d270479808f10ee4799911f8f97abf7ba416cbf
Niko <a1825704@student.adelaide.edu.au>
2025-06-20 03:53:09 +0000

    [automatic commit] 2025-06-20 03:53:09 UTC 101.174.25.68

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -6,7 +6,7 @@ const pool = req.pool;
 
 try {
     pool.query('SELECT Dogs.name, Dogs.size, Users.username FROM Dogs INNER JOIN ON Users.user_id = Dogs.owner_id', (err, results) => {
-        res.json(results[0]);
+        res.send(results[0]);
     });
 } catch(err) {
     res.status(500).send('Error retrieving data: ' + err);

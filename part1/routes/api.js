cfd17c97457a3f7910cd815f96c8bc66b8f54ce0
Niko <a1825704@student.adelaide.edu.au>
2025-06-20 04:09:51 +0000

    [automatic commit] 2025-06-20 04:09:51 UTC 101.174.25.68

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -17,7 +17,7 @@ router.get('/api/walkrequests/open', function(req, res, next) {
 const pool = req.pool;
 
 try {
-    pool.query(`SELECT WalkRequests.request_id, Dogs.name AS dog_name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username AS owner_name FROM WalkRequests WHERE status='open' INNER JOIN Dogs ON Dogs.dog_id = WalkRequests.dog_id INNER JOIN Users ON Dogs.owner_id = Users.user_id',
+    pool.query(`SELECT WalkRequests.request_id, Dogs.name AS dog_name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username AS owner_name FROM WalkRequests WHERE status='open' INNER JOIN Dogs ON Dogs.dog_id = WalkRequests.dog_id INNER JOIN Users ON Dogs.owner_id = Users.user_id`,
         (err, results) => {
             res.send(results);
         });

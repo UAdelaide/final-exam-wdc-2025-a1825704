e25fda8898502c7c065bb9128af8f309917706c7
Niko <a1825704@student.adelaide.edu.au>
2025-06-20 04:07:28 +0000

    [automatic commit] 2025-06-20 04:07:28 UTC 101.174.25.68

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -18,7 +18,9 @@ const pool = req.pool;
 
 try {
     pool.query('SELECT WalkRequests.request_id, Dogs.name AS dog_name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username AS owner_name FROM WalkRequests INNER JOIN Dogs ON Dogs.dog_id = WalkRequests.dog_id INNER JOIN Users ON Dogs.owner_id = Users.user_id',
-        (err)
+        (err, results) => {
+            res.send(results);
+        }
     )
 }
 });

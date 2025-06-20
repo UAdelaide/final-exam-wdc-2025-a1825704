5c26a76908a825d385c46670786fadc0c8a2bc73
Niko <a1825704@student.adelaide.edu.au>
2025-06-20 03:05:27 +0000

    [automatic commit] 2025-06-20 03:05:27 UTC 101.174.25.68

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/api/dogs', function(req, res, next) {
-
+const pool = req.pool;
 });
 
 router.get('/api/walkrequests/open', function(req, res, next) {

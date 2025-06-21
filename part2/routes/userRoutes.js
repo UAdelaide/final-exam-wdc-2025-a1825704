5da7ae52641740c45d08a43300c6cf4ae9efb52a
Niko <a1825704@student.adelaide.edu.au>
2025-06-21 05:08:23 +0000

    [automatic commit] 2025-06-21 05:08:23 UTC 101.174.25.68

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -2,6 +2,22 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
 
+router.get('/api/dogs', function(req, res, next) {
+const pool = req.pool;
+
+try {
+    pool.query('SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username FROM Dogs INNER JOIN Users ON Users.user_id = Dogs.owner_id', (err, results) => {
+        if (err) {
+            console.log('Error Fetching Dogs:', err);
+            return res.status(500).send('Could not load dogs');
+        }
+        res.send(results);
+    });
+} catch(err) {
+    res.status(500).send('Error retrieving data: ' + err);
+}
+});
+
 router.get('/', function (req, res) {
     return res.redirect('/users/login');
 });

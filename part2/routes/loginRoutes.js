f4f701105033411263bd6b704a2f96cbb99aff27
Niko <a1825704@student.adelaide.edu.au>
2025-06-21 01:54:58 +0000

    [automatic commit] 2025-06-21 01:54:58 UTC 101.174.25.68

--- part2/routes/loginRoutes.js
+++ part2/routes/loginRoutes.js
@@ -1,9 +1,5 @@
 var express = require('express');
 var router = express.Router();
-const bcrypt = require('bcrypt');
-const { OAuth2Client } = require('google-auth-library');
-const CLIENT_ID = process.env.client_ID;
-const client = new OAuth2Client(CLIENT_ID);
 const { body, validationResult } = require('express-validator');
 
 // Standard username/password login

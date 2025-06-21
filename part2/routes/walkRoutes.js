20ce5273b4e965c8c6ddfd3de5041ae74dfe241d
Niko <a1825704@student.adelaide.edu.au>
2025-06-21 02:02:28 +0000

    [automatic commit] 2025-06-21 02:02:28 UTC 101.174.25.68

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -4,6 +4,7 @@ const db = require('../models/db');
 
 // GET all walk requests (for walkers to view)
 router.get('/', async (req, res) => {
+  if(req.session.user)
   try {
     const [rows] = await db.query(`
       SELECT wr.*, d.name AS dog_name, d.size, u.username AS owner_name

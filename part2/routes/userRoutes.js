5fc15f5ae27182edca3a55855350b5b8c60f6bde
Niko <a1825704@student.adelaide.edu.au>
2025-06-21 02:11:39 +0000

    [automatic commit] 2025-06-21 02:11:39 UTC 101.174.25.68

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -4,7 +4,9 @@ const db = require('../models/db');
 
 // GET all users (for admin/testing)
 router.get('/', async (req, res) => {
-  if (!req.session.user) return res.status(401).send("Not logged in");
+  if (!req.session.user) {
+        return res.redirect('/users/login');
+    }
   try {
     const [rows] = await db.query('SELECT user_id, username, email, role FROM Users');
     res.json(rows);
@@ -15,7 +17,9 @@ router.get('/', async (req, res) => {
 
 // POST a new user (simple signup)
 router.post('/register', async (req, res) => {
-  if (!req.session.user) return res.status(401).send("Not logged in");
+  if (!req.session.user) {
+        return res.redirect('/users/login');
+    }
   const { username, email, password, role } = req.body;
 
   try {

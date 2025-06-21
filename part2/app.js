2b9d54d42ea84c25a64f5eef51c8964967418bfd
Niko <a1825704@student.adelaide.edu.au>
2025-06-21 02:14:23 +0000

    [automatic commit] 2025-06-21 02:14:23 UTC 101.174.25.68

--- part2/app.js
+++ part2/app.js
@@ -15,5 +15,8 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/walk', walkRoutes);
 app.use('/api/user', userRoutes);
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 // Export the app instead of listening here
 module.exports = app;

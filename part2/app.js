fd25eae815c99e9b1ccf3bcc8f1af2eabf9e9536
Niko <a1825704@student.adelaide.edu.au>
2025-06-21 04:09:26 +0000

    [automatic commit] 2025-06-21 04:09:26 UTC 101.174.25.68

--- part2/app.js
+++ part2/app.js
@@ -3,7 +3,7 @@ const path = require('path');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var engines = require('consolidate');
+
 
 require('dotenv').config();
 
@@ -14,6 +14,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 // Routes

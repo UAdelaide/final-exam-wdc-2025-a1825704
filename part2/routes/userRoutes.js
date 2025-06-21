82e6426c54831dd1f19e38db0827b7b43a3b59f2
Niko <a1825704@student.adelaide.edu.au>
2025-06-21 05:20:24 +0000

    [automatic commit] 2025-06-21 05:20:24 UTC 101.174.25.68

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -77,7 +77,7 @@ router.post('/login', async (req, res) => {
 // GET /users/login
 router.get('/login', async (req, res) => {
   try {
-    const dogs = await db.query
+    const dogs = await db.query(`SELECT * FROM DOGS`);
     res.render('login', { dogs });
 } catch(err) {
     res.status(500).send('Error retrieving data: ' + err);

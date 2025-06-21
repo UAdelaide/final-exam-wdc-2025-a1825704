50241ffc00579d9893d9d1886b331111e1538c7c
Niko <a1825704@student.adelaide.edu.au>
2025-06-21 04:44:44 +0000

    [automatic commit] 2025-06-21 04:44:44 UTC 101.174.25.68

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -79,6 +79,8 @@ router.get('/dogs', async (req, res) => {
 
     const [rows] = await db.query(`SELECT * FROM Dogs WHERE owner_id = ?`, [ownerId]);
 
+
+
     res.json(dogs);
   } catch (err) {
     console.error(err);

e092ae7f2100cf99b426af19e0ced49238cc1de3
Niko <a1825704@student.adelaide.edu.au>
2025-06-21 03:53:02 +0000

    [automatic commit] 2025-06-21 03:53:02 UTC 101.174.25.68

--- part2/app.js
+++ part2/app.js
@@ -30,6 +30,7 @@ app.use(session({
 app.use('/', walkRoutes);
 app.use('/users', userRoutes);
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 

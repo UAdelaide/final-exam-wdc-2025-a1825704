931df5aa5514846a9905354ac339f503609d303b
Niko <a1825704@student.adelaide.edu.au>
2025-06-21 02:41:38 +0000

    [automatic commit] 2025-06-21 02:41:38 UTC 101.174.25.68

--- part2/app.js
+++ part2/app.js
@@ -1,19 +1,37 @@
 const express = require('express');
 const path = require('path');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
 require('dotenv').config();
 
 const app = express();
 
 // Middleware
+app.use(logger('dev'));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
 
-app.use('/api/walks', walkRoutes);
-app.use('/api/users', userRoutes);
+app.use(session({
+  secret: 'hello',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+app.use('/', walkRoutes);
+app.use('/users', userRoutes);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // Export the app instead of listening here
 module.exports = app;

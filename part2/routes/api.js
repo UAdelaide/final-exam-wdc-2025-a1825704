a39030e8a833521e5885226188bd8d729aa47221
Niko <a1825704@student.adelaide.edu.au>
2025-06-21 03:59:31 +0000

    [automatic commit] 2025-06-21 03:59:31 UTC 101.174.25.68

--- part2/routes/api.js
+++ part2/routes/api.js
@@ -3,7 +3,6 @@ var router = express.Router();
 const db = require('../models/db');
 
 router.get('/api/dogs', function(req, res, next) {
-const pool = req.pool;
 
 try {
     db.query('SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username FROM Dogs INNER JOIN Users ON Users.user_id = Dogs.owner_id', (err, results) => {
@@ -22,7 +21,8 @@ router.get('/api/walkrequests/open', function(req, res, next) {
 const pool = req.pool;
 
 try {
-    pool.query(`SELECT WalkRequests.request_id, Dogs.name AS dog_name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username AS owner_username FROM WalkRequests INNER JOIN Dogs ON Dogs.dog_id = WalkRequests.dog_id INNER JOIN Users ON Dogs.owner_id = Users.user_id WHERE status='open'`,
+    db.query(
+        `SELECT WalkRequests.request_id, Dogs.name AS dog_name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username AS owner_username FROM WalkRequests INNER JOIN Dogs ON Dogs.dog_id = WalkRequests.dog_id INNER JOIN Users ON Dogs.owner_id = Users.user_id WHERE status='open'`,
         (err, results) => {
             if (err) {
                 console.log('Error Fetching Open Walkers:', err);
